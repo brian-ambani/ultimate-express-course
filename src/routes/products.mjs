e889b23798c3c3d3ad67d118a7ce1ca9e5c97d31
brian-ambani <brainambani1@gmail.com>
2024-01-09 03:54:14 +0300

    updated the base URL from /api/users to /users

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/api/products", (req, res) => {
+router.get("/products", (req, res) => {
   res.send([
     { id: 123, name: "product1", price: 100, description: "this is product 1" },
   ]);
