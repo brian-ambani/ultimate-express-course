ef158876096035302a5e2edb747e7fe077ac58db
brian-ambani <brainambani1@gmail.com>
2024-01-09 03:33:49 +0300

    updated the constant file

---
+++ src/utils/constants.mjs
@@ -0,0 +1,9 @@
+export const Users = [
+  { id: 1, username: "brain", displayName: "Brain" },
+  { id: 2, username: "mike", displayName: "Mike" },
+  { id: 3, username: "jane", displayName: "Jane" },
+  { id: 4, username: "joe", displayName: "Joe" },
+  { id: 5, username: "james", displayName: "James" },
+  { id: 6, username: "micah", displayName: "Micah" },
+  { id: 7, username: "john", displayName: "John" },
+];
